d9eff843833674e84ad3537362c5a19e96a46294
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-05-17 12:36:41 +0200

    import error fix

---
+++ multirobot_navigation/multirobot_navigation/main.py
@@ -0,0 +1,6 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+if __name__ == '__main__':
+    pass
--- multirobot_navigation/setup.py
+++ multirobot_navigation/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 import os
-import glob
+from glob import glob
 package_name = 'multirobot_navigation'
 
 setup(
